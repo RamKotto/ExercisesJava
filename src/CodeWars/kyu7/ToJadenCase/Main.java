310039777ebb464f5fd835abe00bb6aac79cee8b
RamKotto <black8.tabls@gmail.com>
2021-06-25 12:57:52 +0300

    Some interesting solutions

--- src/CodeWars/kyu7/ToJadenCase/Main.java
+++ src/CodeWars/kyu7/ToJadenCase/Main.java
@@ -25,6 +25,30 @@ public class Main {
 
 /*
 Интересные решения:
+======================================================================
+  public String toJadenCase(String phrase) {
+    if(phrase == null || phrase.equals("")) return null;
 
+    char[] array = phrase.toCharArray();
+
+    for(int x = 0; x < array.length; x++) {
+      if(x == 0 || array[x-1] == ' ') {
+        array[x] = Character.toUpperCase(array[x]);
+      }
+    }
+
+    return new String(array);
+  }
+
+======================================================================
+    public String toJadenCase(String phrase) {
+      if (null == phrase || phrase.length() == 0) {
+          return null;
+      }
+
+      return Arrays.stream(phrase.split(" "))
+                   .map(i -> i.substring(0, 1).toUpperCase() + i.substring(1, i.length()))
+                   .collect(Collectors.joining(" "));
+  }
 
  */
